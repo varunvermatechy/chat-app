a4cc5ca64c8a120b591be8835cd217045343cf3e
Gaurav <gauravtechy123@gmail.com>
2020-09-07 13:25:00 +0530

    chat application in angular 10

---
+++ nodeSocket/server.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { StreamChat } = require('stream-chat');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const serverSideClient = new StreamChat(
+  "ef4ts7cwbnek","w35uf76x6vsdmxkejgh3qxpredukbs8stsa3vf8jk588meye55shrnvmx3bg3q9a");
+console.log('server side',serverSideClient);
+
+app.post('/join', async (req, res) => {
+  const { username } = req.body;
+  const token = serverSideClient.createToken(username);
+  try {
+    await serverSideClient.updateUser(
+      {
+        id: username,
+        name: username,
+      },
+      token
+    );
+  } catch (err) {
+    console.log(err);
+  }
+
+  // const admin = { id: 'admin' };
+  // const channel = serverSideClient.channel('team', 'talkshop', {
+  //   name: 'Talk Shop',
+  //   created_by: admin,
+  // });
+
+  // try {
+  //   await channel.create();
+  //   await channel.addMembers([username, 'admin']);
+  // } catch (err) {
+  //   console.log(err);
+  // }
+
+  return res
+    .status(200)
+    .json({ user: { username }, token, api_key: "ef4ts7cwbnek" });
+});
+
+const server = app.listen(process.env.PORT || 5500, () => {
+  const { port } = server.address();
+  console.log(`Server running on PORT ${port}`);
+});
+
